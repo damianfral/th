af79e3b529d1db37b9360997f64e758468d9df7e
damianfral <huevofritopamojarpan@gmail.com>
2013-11-04 06:51:44 +0100

    CLI added.

--- th.hs
+++ th.hs
@@ -2,141 +2,159 @@
 
 module Main where
 
-import System.Random
+import System.Console.GetOpt
 import System.IO
 import Control.Monad
-import Control.Applicative
 import Data.List
---import Text.Peggy
+import System.Environment
+import System.Directory
+import Control.Monad.Error
 
-data TaskState = NotDone | Started | Done
-	deriving (Eq)
+data TaskState = NotDone | Started | Done deriving (Eq)
 
 instance Show TaskState where
 	show NotDone = "[ ]"
-	show Started = "[~]"
-	show Done    = "[X]"
+	show Started = "[-]"
+	show Done    = "[x]"
 
-instance Read TaskState where
-	readsPrec d ['[', x, ']'] =
-		case x of
-			' ' -> [(NotDone, "")]
-			'~' -> [(Started, "")]
-			'X' -> [(Done, "")]
-
-data Task = Task {identifier :: Int, name :: String, state :: TaskState}
-	deriving (Read)
+data Task = Task {state :: TaskState, name :: String} deriving (Eq)
 
 instance Show Task where
-	show (Task _ name state) = show state ++ " " ++ name
-
-instance Eq Task where
-	(==) (Task identifier1 _ _) (Task identifier2 _ _) = identifier1 == identifier2
-
+	show (Task state name) = show state ++ " " ++ name
 
 type TaskList = [Task]
+type TaskID = Int
+
 instance Show TaskList where
-	show (xs) = concat $ intersperse ("\n") $ map show xs
-        -- show (x:xs) = foldr (\x y -> concat [x, "\n", show y]) x xs
-        -- me gustan demasiado los fold :D
+	show (xs) = intercalate "\n" $ map show xs
 
--------------------------------------------------------------------------------
+parseTask :: String -> Either String Task
+parseTask ('[':' ':']':' ':name) = Right $ Task NotDone name
+parseTask ('[':'-':']':' ':name) = Right $ Task Started name
+parseTask ('[':'x':']':' ':name) = Right $ Task Done name
+parseTask _                      = Left "Bad format"
 
-rand :: IO Int
-rand = do
-	newStdGen
-	gen <- getStdGen
-	return $ fst $ randomR (0::Int, 16777216::Int) gen
+parseTaskList' :: [String] -> TaskList -> Either String TaskList
+parseTaskList' [] x = Right x
+parseTaskList' (x:xs) tasks = parseTask x >>= parseTaskList' xs . (append tasks)
+	where append xs x = xs ++ [x]
 
+parseTaskList :: String -> Either String TaskList
+parseTaskList str = parseTaskList' (lines str) []
 
-createTask :: String -> IO Task
-createTask name = do
-		identifier <- rand
-		return $ Task identifier name NotDone
+---------------------------------------------------------------------------------
 
+createTask ::  String -> TaskList -> TaskList
+createTask name list = Task NotDone name :list
 
--- | Como termino una tarea segun pos
-finishTask :: [Task] -> Int -> [Task]
-finishTask (x:[]) i | i == 1 = [x { state = Done }]
-                    | otherwise = [x]
-finishTask (x:xs) 1 = x { state = Done } : xs
-finishTask (x:xs) i | i > 0     = x : finishTask xs (i-1)
-                    | otherwise = x:xs
+markTask :: TaskState -> TaskID -> TaskList -> TaskList
+markTask st 1 (x:xs)  = x { state = st } : xs
+markTask st i (x:xs) 	| i > 1     = x : markTask st (i-1) xs 
+						| otherwise = x:xs
+markTask _ _ list = list
 
--- | como elimino segun identifier -- no funciona actualmente
-deleteTask :: [Task] -> Int -> [Task]
-deleteTask t i = filter ((/= i) . identifier) t
+startTask, finishTask, deleteTask ::  TaskID -> TaskList -> TaskList
+startTask  = markTask Started
+finishTask = markTask Done
 
--------------------------------------------------------------------------------
+deleteTask 1 (x:xs) = xs
+deleteTask i (x:xs)	| i > 1     = x : deleteTask (i-1) xs
+					| otherwise = x:xs
+deleteTask _ list = list
 
-data Command = Create | Finish | Delete | Print | Exit | Unrecognized
-	deriving (Read, Show)
+---------------------------------------------------------------------------------
 
-readCommand :: String -> Command
+data Action = Create String | Start TaskID | Finish TaskID | Delete TaskID | Print deriving Show
 
-readCommand "create" = Create
-readCommand "c"      = Create
+executeAction :: Action -> TaskList -> TaskList
+executeAction (Create name) = createTask name
+executeAction (Start id)    = startTask id
+executeAction (Finish id)   = finishTask id
+executeAction (Delete id)   = deleteTask id
+executeAction _             = id
 
-readCommand "finish" = Finish
-readCommand "f"      = Finish
+---------------------------------------------------------------------------------
 
-readCommand "delete" = Finish
-readCommand "d"      = Finish
-
-readCommand "exit"   = Exit
-readCommand "e"      = Exit
-
-readCommand "print"  = Print
-readCommand "p"      = Print
-
-readCommand _        = Unrecognized
-
-
-executeCommand :: Command -> [Task] -> IO [Task]
-
-executeCommand Unrecognized list = putStrLn "Unrecognized Command" >> return list
-
-executeCommand Create list = return (:list) <*> (prompt "Task name: " >>= createTask)
-
-executeCommand Finish list = (finishTask list . read) <$> prompt "Task Number: "
-
-executeCommand Delete list = (deleteTask list . read) <$> prompt "Task Number: "
-
-executeCommand Print list = printList list >> return list
-
-executeCommand _ list = return list
-
-
-printList :: [Task] -> IO ()
+printList :: TaskList -> IO ()
 printList list = mapM_ printListItem $ zip [1..] list
 
-
 printListItem :: (Int,Task) -> IO()
-printListItem (n, t) = do
-	putStrLn $ show n ++ " - " ++ show t
+printListItem (n, t) = putStrLn $ show n ++ " - " ++ show t
 
--------------------------------------------------------------------------------
+---------------------------------------------------------------------------------
 
-prompt :: String -> IO String
-prompt text = do
-	putStr text
-	hFlush stdout
-	getLine
+processResult :: Options -> Either String TaskList -> IO ()
+processResult _ (Left str)          = putStrLn str
+processResult options (Right tasks) = printList tasks >> (writeFile  (file options) (show tasks))
 
-repl :: [Task] -> IO [Task]
-repl list = do
-	input <- prompt "> "
-	putStrLn ""
-	let command = readCommand input
-	case command of
-		Exit 	-> return list
-		_ 		-> do
-			list' <- executeCommand command list
-			list'' <- repl list'
-			return list''
+parseIfExists' :: String -> Bool -> IO (Either String TaskList)
+parseIfExists' _ False       = return (Right [])
+parseIfExists' filename True = fmap parseTaskList $ readFile filename
+
+parseIfExists :: String -> IO (Either String TaskList)
+parseIfExists filename = doesFileExist filename >>= parseIfExists' filename
+
+---------------------------------------------------------------------------------
+
+data Options = Options
+	{ help :: Bool
+	, version :: Bool
+	, action :: Action
+	, file :: String
+	} deriving Show
+
+defaultOptions :: Options
+defaultOptions = Options 
+	{ help = False
+	, version = False
+	, action = Print
+	, file = "todo.txt"
+	}
+
+options :: [ OptDescr (Options -> Options)]
+options =
+	[ Option "vV" 	["version"] 
+		(NoArg $ \opts -> opts {version = True})
+		"Show the version of th"
+
+	, Option "hH" 	["help"] 
+		(NoArg $ \opts -> opts {help = True})
+		"Show help for th"
+
+	, Option "c"	["create"] 	
+		(ReqArg  (\ arg opts -> opts {action = Create arg}) "TASKNAME")
+		"Create a new task"
+	
+	, Option "s"	["start"] 	
+		(ReqArg  (\ arg opts -> opts {action = Start $ read arg}) "TASKID")
+		"Mark task as started"
+
+	, Option "f"	["finish"] 	
+		(ReqArg  (\ arg opts -> opts {action = Finish $ read arg}) "TASKID")
+		"Mark task as finished"
+
+	, Option "d"	["delete"] 	
+		(ReqArg  (\ arg opts -> opts {action = Delete $ read arg}) "TASKID")
+		"Delete task"
+
+	, Option "l" ["list"]
+		(ReqArg  (\ arg opts -> opts {file = arg}) "FILENAME")
+		"Use this file as the task list"
+	]
+
+parseOptions :: [String] -> Either String Options
+parseOptions argv = case getOpt Permute options argv of
+		(o, n, []  ) 	-> Right (foldl (flip id) defaultOptions o)
+		(_, _, errors) 	-> Left $ concat errors ++ usageInfo header options
+	where header = "\n\nUsage: th [OPTION...]"
+
+---------------------------------------------------------------------------------
 
 main :: IO ()
 main = do
-	putStrLn "## Task list ##"
-	repl []
-	putStrLn ""
+	args <- getArgs
+	case parseOptions args of 
+		Left str -> putStrLn str
+		Right options -> do
+			taskList <- parseIfExists $ file options
+			processResult options $ taskList >>= (Right . executeAction (action options))
